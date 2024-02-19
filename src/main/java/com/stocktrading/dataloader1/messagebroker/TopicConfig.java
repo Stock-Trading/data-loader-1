8309e493447d2979a973172558ff692413142237
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2024-02-19 22:53:18 +0100

    basic configuration of kafka producer

---
+++ src/main/java/com/stocktrading/dataloader1/messagebroker/TopicConfig.java
@@ -0,0 +1,16 @@
+package com.stocktrading.dataloader1.messagebroker;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class TopicConfig {
+
+    @Bean
+    public NewTopic stockTopicFactory() {
+        return TopicBuilder.name("stockInfo")
+                .build();
+    }
+}
