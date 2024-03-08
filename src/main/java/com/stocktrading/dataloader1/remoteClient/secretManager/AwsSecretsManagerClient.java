b506843f09343c25f2abfe515950bab2e83615f6
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2024-03-08 15:37:05 +0100

    refactoring

--- src/main/java/com/stocktrading/dataloader1/remoteClient/secretManager/AwsSecretsManagerClient.java
+++ src/main/java/com/stocktrading/dataloader1/remoteClient/secretManager/AwsSecretsManagerClient.java
@@ -5,33 +5,35 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
 @Component
 @Log4j2
-class RemoteSecretsManagerClientImpl implements RemoteSecretsManagerClient {
+class AwsSecretsManagerClient implements RemoteSecretsManagerClient {
 
+    /**
+     * @param secretName Name of the secret object in AWS Secret Manager
+     * @param secretKey Key for the secret value stored in AWS SM (secret is an AWS SM object stored (in this case) as key-value pair and send in JSON format)
+     * @return secret value
+     */
     @Override
-    public String getSecretFromAws(String secretName, String secretDescription, Region awsRegion) {
+    public String getSecret(String secretName, String secretKey) {
         log.info("Obtaining secret {} from AWS Secret Manager", secretName);
-        try (SecretsManagerClient client = SecretsManagerClient.builder()
-                .region(awsRegion) //TODO zamiast metody builder i definiowania regionów, należy użyć metody create i nie trzeba wtedy definiować
-                .build()) {
+        try (SecretsManagerClient client = SecretsManagerClient.create()) {
             GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
                     .secretId(secretName)
                     .build();
 
             GetSecretValueResponse getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
-            String secretValue = getSecretFromKeyValueJsonPair(getSecretValueResponse.secretString(), secretDescription); //TODO sprawdzić metodę getValueForField
+            String secretValue = getSecretFromKeyValueJsonPair(getSecretValueResponse.secretString(), secretKey);
             log.info("Successfully obtained secret {} from AWS Secret Manager", secretName);
             return secretValue;
         } catch (Exception e) {
             log.error("Could not obtain secret {} from AWS Secret Manager. Exception has been thrown {}", secretName, e);
-            throw new AwsSecretManagerRuntimeException("Could not obtain a secret " + secretName +
-                    " from AWS Secret Manager. Exception has been thrown " + e);
+            throw new SecretManagerClientRuntimeException("Could not obtain secret \"" + secretName +
+                    "\" from AWS Secret Manager. Exception has been thrown " + e);
         }
     }
 
---
+++ src/main/java/com/stocktrading/dataloader1/remoteClient/secretManager/SecretManagerClientRuntimeException.java
@@ -0,0 +1,9 @@
+package com.stocktrading.dataloader1.remoteClient.secretManager;
+
+class SecretManagerClientRuntimeException extends RuntimeException {
+
+    public SecretManagerClientRuntimeException(String message) {
+        super(message);
+    }
+
+}
