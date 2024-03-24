8db64f623beadeb9a428142dc156934b5185ed0b
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2024-03-24 20:35:51 +0100

    TODO

--- src/main/java/com/stocktrading/dataloader1/DataLoader1Application.java
+++ src/main/java/com/stocktrading/dataloader1/DataLoader1Application.java
@@ -20,3 +20,17 @@ public class DataLoader1Application {
 // różne zewnętrzne API będą pobierać te same dane, ale w JSON (czy cokolwiek innego) może być inaczej skonstruowany
 // model domeny jest jeden, modele DTO powinny mapować do jednego modelu, który powinien być już ustalony
 // schemat budowy aplikacji jak w poprzednim projekcie
+
+
+//TODO tworzyć ręcznie klientów websocketowych na żądanie. Domain musi mieć metody kontrolujące tworzenie i niszczenie
+// instancji klientów
+// Metoda tworząca nowe instancje powinna przyjmować nazwę instrumentu finansowego (np. symbol firmy lub parę walutową)
+// do których klient ma subskrybować na jednym websocketowym połączniu. Przyjmujemy, że jedno połączenie obsługuje
+// do pięciu instrumentów (np. 17 zasubskrybowanych instrumentów oznacza 4 połączenia web socketowe, 3x5+2 instrumentów)
+// Każde połączenie ma swój wątek.
+
+//TODO zastanowić się czy to domain ma obsługiwać liczbę klientów czy warstwa klienta (warstwa transportowa) sama to kontroluje
+
+//TODO zobaczyć czy jeden klient web socketowy może nadawać na wiele wątków
+
+//https://docs.spring.io/spring-framework/docs/4.3.5.RELEASE/spring-framework-reference/html/websocket.html#websocket-stomp-configuration-performance
