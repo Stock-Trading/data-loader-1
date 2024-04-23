1f3ea0b55c96d6b5226ea4a4e601ea044a6ac24d
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2024-04-23 21:19:45 +0200

    deleted unused import

--- src/main/java/com/stocktrading/dataloader1/remote/finnHub/FinnHubApiClient.java
+++ src/main/java/com/stocktrading/dataloader1/remote/finnHub/FinnHubApiClient.java
@@ -1,6 +1,5 @@
 package com.stocktrading.dataloader1.remote.finnHub;
 
-import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import okhttp3.OkHttpClient;
