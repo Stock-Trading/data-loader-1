86827091e9c36685814bf1e099f2ef2f190c3a77
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2024-05-19 18:52:50 +0200

    unnecessary annotations deleted

--- src/main/java/com/stocktrading/dataloader1/remote/finnHub/FinnHubApiHandlerFactory.java
+++ src/main/java/com/stocktrading/dataloader1/remote/finnHub/FinnHubApiHandlerFactory.java
@@ -2,13 +2,9 @@ package com.stocktrading.dataloader1.remote.finnHub;
 
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-
-import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
 @Component
 @AllArgsConstructor
