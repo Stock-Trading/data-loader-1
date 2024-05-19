0dbde6fe4c527d5f57186d7c2f8e3c1153412eca
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2024-05-19 22:49:05 +0200

    deleted unnecessary imports

--- src/main/java/com/stocktrading/dataloader1/remote/finnHub/OkHttpClientFactory.java
+++ src/main/java/com/stocktrading/dataloader1/remote/finnHub/OkHttpClientFactory.java
@@ -1,17 +1,12 @@
 package com.stocktrading.dataloader1.remote.finnHub;
 
-import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class OkHttpClientFactory {
 
     public OkHttpClient okHttpClient() {
-
         return new OkHttpClient.Builder()
                 .build();
     }
